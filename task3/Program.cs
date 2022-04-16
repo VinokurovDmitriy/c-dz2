c84bd4a545ff5eb95e488e0ed25987442d7c8dda
dvvinokurov <dmitvitvin@gmail.com>
2022-04-16 13:55:58 +0300

    add all tasks

---
+++ task3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа выводит в консоль третью цифру числа или сообщает что ее нет в числе
+int number = new Random().Next(10,1000);
+if(number / 100 >= 1){
+    Console.WriteLine($"В числе {number}  третья цифра {number%10}");
+} else {
+    Console.WriteLine($"В числе {number} нет третьей цифры");
+}

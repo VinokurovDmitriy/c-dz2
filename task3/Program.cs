f44d7d4c746811601b31bbd0a7a2bd141930486c
dvvinokurov <dmitvitvin@gmail.com>
2022-04-16 18:57:17 +0300

    change condition in task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Программа выводит в консоль третью цифру числа или сообщает что ее нет в числе
 int number = new Random().Next(10,1000);
-if(number / 100 >= 1){
+if(number  > 99){
     Console.WriteLine($"В числе {number}  третья цифра {number%10}");
 } else {
     Console.WriteLine($"В числе {number} нет третьей цифры");

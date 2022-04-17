34bca2bccd1d75ddcbb19b4f6ab2eb62e36824ae
dvvinokurov <dmitvitvin@gmail.com>
2022-04-17 16:18:55 +0300

    change task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,7 +1,22 @@
 ﻿// Программа выводит в консоль третью цифру числа или сообщает что ее нет в числе
-int number = new Random().Next(10,1000);
-if(number  > 99){
-    Console.WriteLine($"В числе {number}  третья цифра {number%10}");
-} else {
+int number = new Random().Next(-99999, 99999);
+// int number = new Random().Next(-99, 99); //для теста двузначных чисел эту строку раскомментировать
+int temp = number;
+if (number < 0)
+{
+    temp = -number;
+}
+if (temp < 100)
+{
     Console.WriteLine($"В числе {number} нет третьей цифры");
 }
+else
+{
+    while (temp > 999)
+    {
+        temp /= 10;
+    }
+    Console.WriteLine($"В числе {number}  третья цифра {temp % 10}");
+}
+
+
